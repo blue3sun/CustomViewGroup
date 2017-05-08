3ca72732d20ab7db36e28c26c627f274dc7f9ee9
zenglanjing <zenglanjing@bravowhale.com>
2017-05-08 18:09:30 +0800

    实现customviewgroup

--- app/src/main/java/com/monkey/customviewgroup/CustomViewGroup.java
+++ app/src/main/java/com/monkey/customviewgroup/CustomViewGroup.java
@@ -1,7 +1,10 @@
 package com.monkey.customviewgroup;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
 
 /**
@@ -11,6 +14,8 @@ import android.view.ViewGroup;
  */
 
 public class CustomViewGroup extends ViewGroup {
+
+    private int interval = 60;
 
     public CustomViewGroup(Context context) {
         super(context);
@@ -25,7 +30,33 @@ public class CustomViewGroup extends ViewGroup {
     }
 
     @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec,heightMeasureSpec);
+        int childCount = getChildCount();
+        for(int i=0;i<childCount;i++){
+            View child = getChildAt(i);
+            child.measure(widthMeasureSpec,heightMeasureSpec);
+        }
+    }
 
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int buttom) {
+        int childCount = getChildCount();
+        for(int i=0;i<childCount;i++){
+            View child = getChildAt(i);
+            int measureWidth = child.getMeasuredWidth();
+            int measureHeight = child.getMeasuredHeight();
+            Log.e("layout", "measureWidth: "+measureWidth +"   measureHeight"+measureHeight);
+            if(i>0){
+                left = left + interval;
+                top = top + measureHeight;
+            }
+            child.layout(left,top,left+measureWidth,top+measureHeight);
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
     }
 }
